960fd3dbfc09fd86f1f40bfac5a4f86892472bd6
ASelimK <selim8karakus@gmail.com>
2022-03-18 19:14:49 +0300

    JavaScript Basics - functions, variables, some basic syntax

--- site/js/script.js
+++ site/js/script.js
@@ -1,1 +1,13 @@
-var x = "Bismillahirrahmanirrahim"
+var message = "Bismillahirrahmanirrahim";
+console.log("global: message = " + message);
+
+var a = function(){
+    var message = "inside a";
+    console.log("a: message = " + message);
+    function b () {
+        console.log("b: message = " + message);
+    }
+    b()
+}
+
+a()
