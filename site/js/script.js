2b0c1ab96c9e30eaf9174c515830c146314abff3
ASelimK <selim8karakus@gmail.com>
2022-03-19 23:30:46 +0300

    JS objects and functions playground

--- site/js/script.js
+++ site/js/script.js
@@ -1,13 +1,81 @@
-var message = "Bismillahirrahmanirrahim";
-console.log("global: message = " + message);
+var aks = new Object();
+aks.index = 3;
+aks.index.type = "1. bölgede";
+aks.rotation = "small";
+console.log(aks["rotation"]); 
 
-var a = function(){
-    var message = "inside a";
-    console.log("a: message = " + message);
-    function b () {
-        console.log("b: message = " + message);
+var x = {
+    name: "adf",
+    dsdfa: "gg",
+    rr : 2,
+    ratata: {
+        fname: "asdf",
+        lname: "adfg"
     }
-    b()
+};
+console.log(x)
+
+function hifive(x){
+    return x*5;
 }
 
-a()
+hifive.version = "v.1.0.0";
+console.log(hifive);
+console.log(hifive.toString());
+console.log(hifive.version);
+
+function makeMultiplier(multiply){
+    var myFunc = function(x){
+        return multiply * x;
+    };
+    return myFunc ;
+}
+
+var mb3 = makeMultiplier(3);
+console.log(mb3);  
+
+
+var a, b, c, d, e;
+a = 7;
+b = 7;
+b+=8;
+console.log(a);
+console.log(b);
+
+// console.log();
+// console.log();
+// console.log();
+
+function circle(radius){
+    console.log(this);
+}
+var circ = new circle(10);
+console.log(circ);
+
+function blue(lyrics){
+    this.lyrics = lyrics;
+}
+
+blue.prototype.getlyrics = function (){
+    return this.lyrics;}
+
+var x = new blue("almost");
+console.log(x);
+console.log("-------------------");
+
+
+var hexagon = {
+    length: 10,
+
+    getArea: function(){
+        var self = this;
+        console.log(this.length);
+    
+        var increaseRadius = function (){
+            this.length = 230;
+        };
+        increaseRadius();
+        console.log(this);
+    }   
+};
+console.log(hexagon.getArea());
