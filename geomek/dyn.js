b3b395da9c45d6c301d992be175a12586d734184
ASelimK <selim8karakus@gmail.com>
2022-03-24 13:41:35 +0300

    With <defs> i grouped main 6star shape.

--- geomek/dyn.js
+++ geomek/dyn.js
@@ -1,6 +1,6 @@
 
 
 // document.getElementById("north").transform = "rotate(60,140,103)"
-document.getElementById("north").transform = "rotate(60)"
+document.getElementById("north").transform = "scale(2 1.5)"
 
 butto
